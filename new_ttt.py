a14a89952222dc058fe4d12eee56c09d3f863236
brandonn <brandonn@sfu.ca>
2019-03-02 20:46:54 -0800

    refactored, still need to fix some bugs. Computer will take over Player's square

--- new_ttt.py
+++ new_ttt.py
@@ -7,46 +7,42 @@ import os
 import time
 import random
 
-# Declaring the Game board, extra space at the top is to 1-index the game board
-board = [" ",
-         " "," "," ",
-         " "," "," ",
-         " "," "," "]
-
 # Defining a header with starting information
-def print_header():
+def PrintHeader():
+    os.system("clear")
     print ("""Welcome to my Tic-Tac-Toe game!
-              These are the corresponding numerical values for each board position:
+These are the corresponding numerical values for each board position:
 
-              1|2|3
-              -----
-              4|5|6
-              -----
-              7|8|9""")
+0|1|2
+-----
+3|4|5
+-----
+6|7|8""")
 
 # Print the current board and piece positions
-def print_board():
-    print ("Current game: ")
-    print (board[1] + "|" + board[2] + "|" + board[3])
+def PrintBoard():
+    PrintHeader()
+    print ("\nCurrent game: ")
+    print (m_board[0] + "|" + m_board[1] + "|" + m_board[2])
     print ("-----")
-    print (board[4] + "|" + board[5] + "|" + board[6])
+    print (m_board[3] + "|" + m_board[4] + "|" + m_board[5])
     print ("-----")
-    print (board[7] + "|" + board[8] + "|" + board[9])
+    print (m_board[6] + "|" + m_board[7] + "|" + m_board[8])
 
 # Choose player piece X or O
-def goes_first():
-    user = input("Do you want to go first? y/n")
+def GoesFirst():
+    user = input("Do you want to go first? y/n\n")
     if user == "y" or user == "Y":
         return True
     elif user == "n" or user == "N":
         return False
     else:
         print("Please choose a valid option.")
-        goes_first()
+        GoesFirst()
 
 # Choose computer difficulty
-def choose_difficulty():
-    difficulty = input("Please choose computer difficulty: 1 = EASY, 2 = INTERMEDIATE, 3 = HARD")
+def ChooseDifficulty():
+    difficulty = input("Please choose computer difficulty: 1 = EASY, 2 = INTERMEDIATE, 3 = HARD\n")
     if difficulty == "1":
         return 2
     elif difficulty == "2":
@@ -54,75 +50,76 @@ def choose_difficulty():
     elif difficulty == "3":
         return 6
     else:
-        print("Please choose a valid difficulty level.")
-        choose_difficulty()
+        print("Please choose a valid difficulty level.\n")
+        ChooseDifficulty()
 
 # Set up the game difficulty and player pieces 
-def setup_game():
-    return goes_first(), choose_difficulty()
+def SetupGame():
+    return GoesFirst(), ChooseDifficulty()
     
 # Checking if given player has won with given board
-def win(board, player):
+def Win(board, player):
     # first row
-    if board[1] == player[1] and board[2] == player[1] and board[3] == player[1]:
+    if board[0] == player[1] and board[1] == player[1] and board[2] == player[1]:
         return True
     # second row
-    elif board[4] == player[1] and board[5] == player[1] and board[6] == player[1]:
+    elif board[3] == player[1] and board[4] == player[1] and board[5] == player[1]:
         return True
     # last row
-    elif board[7] == player[1] and board[8] == player[1] and board[9] == player[1]:
+    elif board[6] == player[1] and board[7] == player[1] and board[8] == player[1]:
         return True
     # fist column
-    elif board[1] == player[1] and board[4] == player[1] and board[7] == player[1]:
+    elif board[0] == player[1] and board[3] == player[1] and board[6] == player[1]:
         return True
     # second column
-    elif board[2] == player[1] and board[5] == player[1] and board[8] == player[1]:
+    elif board[1] == player[1] and board[4] == player[1] and board[7] == player[1]:
         return True
     # third column
-    elif board[3] == player[1] and board[6] == player[1] and board[9] == player[1]:
+    elif board[2] == player[1] and board[5] == player[1] and board[8] == player[1]:
         return True
     # \ - diagonal
-    elif board[1] == player[1] and board[5] == player[1] and board[9] == player[1]:
+    elif board[0] == player[1] and board[4] == player[1] and board[8] == player[1]:
         return True
     # / - diagonal
-    elif board[3] == player[1] and board[5] == player[1] and board[7] == player[1]:
+    elif board[2] == player[1] and board[4] == player[1] and board[6] == player[1]:
         return True
 
 # Checking if given board evaluates to a tie between both players
-def tie(board):
-    if (not win(board, player[0]) and not win(board, player[1])):
-        return True
-    else:
-        return False
+def Tie(board):
+    filled = True
+    for i in board:
+        if i == " ":
+            filled = False
+    return filled
 
 # Getting empty positions
-def get_available_moves(thisBoard):
-    available_moves = [None]
-    for i, val in enumerate(thisBoard):
+def GetAvailableMoves(board):
+    availableMoves = []
+    for i, val in enumerate(board):
         if val == " ":
             # add position to list if the cell is empty
-            available_moves.append(i)
-    return available_moves
+            availableMoves.append(i)
+    return availableMoves
 
-def evaluateScore(thisBoard, cell_1, cell_2, cell_3):
+def EvaluateScore(board, cell_1, cell_2, cell_3):
     # bestScore = 1, if line starts with "O"
     # bestScore = -1, if line starts with "X"
-    if thisBoard[cell_1] == "O":
+    if board[cell_1] == "O":
         bestScore = 1
-    elif thisBoard[cell_1] == "X":
+    elif board[cell_1] == "X":
         bestScore = -1
     else:
         bestScore = 0
 
     # bestScore times 10 if 2nd piece in the line matches 1st piece
-    if thisBoard[cell_2] == "O":
+    if board[cell_2] == "O":
         if bestScore == 1:
             bestScore *= 10
         elif bestScore == -1:
             bestScore =  0
         else:
             bestScore = 1
-    elif thisBoard[cell_2] == "X":
+    elif board[cell_2] == "X":
         if bestScore == -1:
             bestScore *= 10
         elif bestScore == 1:
@@ -131,14 +128,14 @@ def evaluateScore(thisBoard, cell_1, cell_2, cell_3):
             bestScore = -1
 
     #bestScore times 10 again if 3rd piece matches 2nd and 1st pieces
-    if thisBoard[cell_3] == "O":
+    if board[cell_3] == "O":
         if bestScore > 0:
             bestScore *= 10
         elif bestScore < 0:
             return 0
         else:
             bestScore = 1
-    elif thisBoard[cell_3] == "X":
+    elif board[cell_3] == "X":
         if bestScore < 0:
             bestScore *= 10
         elif bestScore > 0:
@@ -149,140 +146,118 @@ def evaluateScore(thisBoard, cell_1, cell_2, cell_3):
     return bestScore
 
 # Evaluate score of a board
-def evaluate(thisBoard):
+def EvaluateBoard(board):
     bestScore = 0
-    currentBoard = list(thisBoard)
+    tmpBoard = list(board)
 
     # Horizontal lines
-    bestScore += evaluateScore(currentBoard, 1, 2, 3)
-    bestScore += evaluateScore(currentBoard, 4, 5, 6)
-    bestScore += evaluateScore(currentBoard, 7, 8, 9)
+    bestScore += EvaluateScore(tmpBoard, 0, 1, 2)
+    bestScore += EvaluateScore(tmpBoard, 3, 4, 5)
+    bestScore += EvaluateScore(tmpBoard, 6, 7, 8)
 
     # Vertical lines
-    bestScore += evaluateScore(currentBoard, 1, 4, 7)
-    bestScore += evaluateScore(currentBoard, 2, 5, 8)
-    bestScore += evaluateScore(currentBoard, 3, 6, 9)
+    bestScore += EvaluateScore(tmpBoard, 0, 3, 6)
+    bestScore += EvaluateScore(tmpBoard, 1, 4, 7)
+    bestScore += EvaluateScore(tmpBoard, 2, 5, 8)
 
     # Diagnal lines
-    bestScore += evaluateScore(currentBoard, 1, 5, 9)
-    bestScore += evaluateScore(currentBoard, 3, 5, 7)
+    bestScore += EvaluateScore(tmpBoard, 0, 4, 8)
+    bestScore += EvaluateScore(tmpBoard, 2, 4, 6)
 
     return bestScore
 
 # Defining our computer AI
-def minimax(thisBoard, player, depth):
+def Minimax(board, player, depth):
     # Find all available moves for given board
-    newBoard = list(thisBoard)
-    available_moves = get_available_moves(newBoard)
+    tmpBoard = list(board)
+    availableMoves = GetAvailableMoves(tmpBoard)
 
-    if player[0] == "Player":
-        bestScore = float("-inf")
-    else:
+    if player[0] == True:
         bestScore = float("inf")
+    else:
+        bestScore = float("-inf")
 
     currentScore = 0
     bestMove = -1
 
     # Reached the end of recursion, board filled up
-    if not available_moves or depth == 0:
-        bestScore = evaluate(newBoard)
+    if depth == 0 or availableMoves == []:
+        bestScore = EvaluateBoard(tmpBoard)
     # Board not filled up yet
     else:
         # Iterate through possible moves
-        for move in available_moves:
-              # Simulate move on board
-              move = int(move)
-              newBoard[move] = player[1]
-              # After Player moves, determine best move for AI
-              if player[0] == "Player":
-                  if player[1] == "O":
-                      currentScore = minimax(newBoard, ["AI", "X"], depth-1)[0]
-                      if currentScore > bestScore:
-                          bestScore = currentScore
-                          bestMove = move
-                  else:
-                      currentScore = minimax(newBoard, ["AI", "O"], depth-1)[0]
-                      if currentScore < bestScore:
-                          bestScore = currentScore
-                          bestMove = move
-                  # Reset board after simulation
-                  newBoard[move] = " "
-              # After AI moves, determine best move for Player
-              else:
-                  if player[1] == "X":
-                      currentScore = minimax(newBoard, ["Player", "O"], depth-1)[0]
-                      if currentScore > bestScore:
-                          bestScore = currentScore
-                          bestMove = move
-                  else:
-                      currentScore = minimax(newBoard, ["Player", "X"], depth-1)[0]
-                      if currentScore < bestScore:
-                          bestScore = currentScore
-                          bestMove = move
-                  newBoard[move] = " "
+        for i in range(len(availableMoves)):
+                # Simulate move on board
+                tmpBoard[availableMoves[i]] = player[1]
+                # After Player moves, determine best move for AI
+                if player[0] == True:
+                    currentScore = Minimax(tmpBoard, m_computer, depth-1)[0]
+                    if currentScore < bestScore:
+                        bestScore = currentScore
+                        bestMove = i
+                # After AI moves, determine best move for Player
+                else:
+                    currentScore = Minimax(tmpBoard, m_player, depth-1)[0]
+                    if currentScore > bestScore:
+                        bestScore = currentScore
+                        bestMove = i
+                tmpBoard[availableMoves[i]] = " "
 
     return (bestScore, bestMove)
 
 # Place given player's piece on given board, player move is input and computer move is result of minimax
-def placeTurn(board, player, depth):
+def PlaceTurn(player, depth):
     # Prompt player for a move
-    if depth == None:
+    if player[0] == True:
         position = int(input("Select a spot: "))
-        if board[position] == " ":
-            board[position] = player[1]
-            os.system("clear")
-            print_board()
+        if m_board[position] == " ":
+            m_board[position] = player[1]
         else:
             print("This spot is taken, please pick another spot.")
-            placeTurn(board, player, depth)
+            PlaceTurn(player, depth)
     # Find best move for AI
     else:
-        position = minimax(board, player, depth)[1]
-        board[position] = player[1]
+        position = Minimax(m_board, player, depth)[1]
+        m_board[position] = player[1]
+        print("Placing computer piece")
 
-def play_game(player, computer, playerTurn, depth):
-    while True:
-        os.system("clear")
-        print_header()
-        print_board()
+def PlayGame(playerTurn, depth):
+    if Win(m_board, m_player):
+        return print("Player wins!\n")
+    elif Win(m_board, m_computer):
+        return print("Computer wins!\n")
+    elif Tie(m_board):
+        return print("It's a tie!\n")
 
-        if win(board, player):
-            os.system("clear")
-            print_header()
-            print_board()
-            print("Player wins!")
-            break
-        elif win(board, computer):
-            os.system("clear")
-            print_header()
-            print_board()
-            print("Computer wins!")
-            break
-        elif tie(board):
-            os.system("clear")
-            print_header()
-            print_board()
-            print("It's a tie!")
-            break
-    
-        if playerTurn == True:
-            placeTurn(board, player, depth)
-            print_board()
-            playerTurn = False
-        else:
-            placeTurn(board, computer, depth)
-            print_board()
-            playerTurn = True
+    if playerTurn == True:
+        print("Play Game for player")
+        PlaceTurn(m_player, depth)
+        PrintBoard()
+        PlayGame(False, depth)
+    else:
+        print("Play Game for computer")
+        PlaceTurn(m_computer, depth)
+        PrintBoard()
+        PlayGame(True, depth)
 
+###
 ### Main Section
-print_header()
-playerTurn, depth = setup_game()
+###
 
-if playerTurn == True:
-    player = ["Player", "X"]
-    computer = ["AI", "O"]
-    play_game(player, computer, playerTurn, depth)
+# Declaring the Game board, extra space at the top is to 1-index the game board
+m_board = [" "," "," ",
+           " "," "," ",
+           " "," "," "]
+
+PrintHeader()
+m_playerTurn, m_depth = SetupGame()
+
+if m_playerTurn == True:
+    m_player = [True, "X"]
+    m_computer = [False, "O"]
 else:
-    player = ["Player", "O"]
-    computer = ["AI", "X"]
-    play_game(player, computer, playerTurn, depth)
+    m_player = [True, "O"]
+    m_computer = [False, "X"]
+
+PrintBoard()
+PlayGame(m_playerTurn, m_depth)
